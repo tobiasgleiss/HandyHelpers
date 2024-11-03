cf8af44dd9940d40dab679b4b5b81c7788da1e8a
Tobias Gleiss <tobias@gleiss.me>
2024-11-03 21:16:03 +0100

    [PI-004] Improve .hidden modifer, set minimum versions

--- Sources/HandyHelpers/Modifier/HiddenModifier.swift
+++ Sources/HandyHelpers/Modifier/HiddenModifier.swift
@@ -7,45 +7,37 @@
 
 import SwiftUI
 
+@available(iOS 17.0, *)
+@available(macOS 14.0, *)
 extension View {
-
+    
     /// Hide or show the view based on a boolean value.
     /// - Parameters:
     ///   - isHidden: Boolean value indicating whether or not to hide the view
-    ///   - remove: Boolean value indicating whether or not to remove the view (view must also be hidden to be removed)
+    ///   - andRemoved: Boolean value indicating whether or not to remove the view (view must also be hidden to be removed)
     /// - Returns: The given view shown or hidden/removed.
-    ///
-    /// Example for visibility:
-    /// ```
-    /// Text("Label")
-    ///     .hidden(true)
-    /// ```
-    ///
-    /// Example for complete removal:
-    /// ```
-    /// Text("Label")
-    ///     .hidden(true, remove: true)
-    /// ```
-    @discardableResult func hidden(_ isHidden: Bool, andRemoved remove: Bool = false) -> some View {
-        modifier(HiddenModifier(isHidden: isHidden, remove: remove))
+    @discardableResult func hidden(_ isHidden: Bool, andRemoved: Bool = false) -> some View {
+        modifier(HiddenModifier(isHidden: isHidden, andRemoved: andRemoved))
     }
-
+    
 }
 
+@available(iOS 17.0, *)
+@available(macOS 14.0, *)
 private struct HiddenModifier: ViewModifier {
 
     private let isHidden: Bool
-    private let remove: Bool
+    private let andRemoved: Bool
 
-    init(isHidden: Bool, remove: Bool = false) {
+    init(isHidden: Bool, andRemoved: Bool = false) {
         self.isHidden = isHidden
-        self.remove = remove
+        self.andRemoved = andRemoved
     }
 
     func body(content: Content) -> some View {
         Group {
             if isHidden {
-                if remove {
+                if andRemoved {
                     EmptyView()
                 } else {
                     content
