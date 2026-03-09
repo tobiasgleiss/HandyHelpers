ee3bccd0fdaa40c8e7e251162b5e38c59d1f7fc9
Tobias Gleiss <tobias@gleiss.me>
2026-03-09 11:16:22 +0100

    [PI-007] Add Array extension .containsAnything

---
+++ Sources/HandyHelpers/Extensions/Array.swift
@@ -0,0 +1,15 @@
+//
+//  Array.swift
+//  
+//
+//  Created by Tobias Gleiss on 09.03.26.
+//
+
+public extension Array {
+    
+    /// Returns if the given array contains any elements.
+    var containsAnything: Bool {
+        !isEmpty
+    }
+    
+}
